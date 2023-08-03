0e3f8d0110865896ce40ce48c46acd8c10f73520
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-08-03 18:21:45 +0300

    Initial

---
+++ src/components/options.js
@@ -0,0 +1,7 @@
+export const baskiRenkSecimi = [
+    { value: 'C', label: 'Cyan (C)' },
+    { value: 'M', label: 'Magenta (M)' },
+    { value: 'Y', label: 'Yellow (Y)' },
+    { value: 'K', label: 'Key (Black) (K)' },
+  ];
+  
